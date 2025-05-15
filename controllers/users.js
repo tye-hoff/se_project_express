1c21e13962c31b38c5a4bbb84ea2a5e31cbbe666
Tyler.Hoffmann <tyler.hoffmann5654@gmail.com>
2025-05-15 11:45:27 -0400

    project update

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,7 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 const errors = require("../utils/errors");
-const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/congif");
 
 const createUser = (req, res) => {
